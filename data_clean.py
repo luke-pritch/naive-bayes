3b07f9e5850660bbc7f013f6e1efde63658b200c
Luke Pritchard <luke-pritch@users.noreply.github.com>
2018-01-09 20:34:18 -0500

    completed script

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,7 @@
 import csv
 import random
+from math import *
+
 
 def loadCsv(filename):
     lines = csv.reader(open(filename, "rt"))
@@ -7,6 +9,7 @@ def loadCsv(filename):
     for i in range(len(dataset)):
         dataset[i] = [float(x) for x in dataset[i]]
     return dataset
+
 
 def splitDataset(dataset, splitRatio):
     trainSize = int(len(dataset) * splitRatio)
@@ -24,3 +27,76 @@ def splitDataset(dataset, splitRatio):
 # dataset = loadCsv(filename)
 # print('Loaded data file {0} with {1} rows'.format(filename, len(dataset)))
 
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+
+def mean(num):
+    return sum(num) / float(len(num))
+
+
+def stdev(num):
+    avg = mean(num)
+    var = sum([pow(x - avg, 2) for x in num]) / float(len(num) - 1)
+
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+
+def calculateClassProbabilities(summaries, inputVector):
+    summaries = dict(summaries)
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
